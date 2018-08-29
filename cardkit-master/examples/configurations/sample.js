fb1f69015a67a764f7c9122e845a8291e86ac957
martin-banks <anotherbanksy@gmail.com>
2018-08-29 18:22:37 +1000

    color swatches

--- cardkit-master/examples/configurations/sample.js
+++ cardkit-master/examples/configurations/sample.js
@@ -1,8 +1,39 @@
 // Base configuration object 
 var colors = [
+  // neutral
+  '#fff',
+  '#ddd',
+  '#888',
   '#333',
+
+  // reds
+  '#f66',
   '#d33',
-  '#25a',
+  '#a00',
+  // oranges
+  '#c40',
+  '#e50',
+  '#f70',
+  // yellows
+  '#fa0',
+  '#fc2',
+  '#ed4',
+  // greens
+  '#af4',
+  '#8d3',
+  '#5a0',
+  // blues
+  '#4bf',
+  '#38f',
+  '#06c',
+  // purples
+  '#538',
+  '#72a',
+  '#a0c',
+  // pinks
+  '#c08',
+  '#d3c',
+  '#e6f',
 ]
 
 var configuration = {
@@ -49,6 +80,9 @@ var configuration = {
           step: 30,
           max: 1080
         },
+        fill: {
+          options: colors,
+        }
         // height: {
         //   min: 100,
         //   max: 2000,
