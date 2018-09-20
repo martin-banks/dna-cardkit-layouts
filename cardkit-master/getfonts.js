5f2f2b6c7f26e5c2610eeee7002ca4b34bc4fa45
martin-banks <anotherbanksy@gmail.com>
2018-09-20 18:51:57 +1000

    adds roboto fonts to cta

--- cardkit-master/getfonts.js
+++ cardkit-master/getfonts.js
@@ -2,7 +2,9 @@ const googlefontcssmerge = require('googlefontcssmerge')
 const googlefontcss64 = require('googlefontcss64')
 const css = require('css')
 
-const url = `https://fonts.googleapis.com/css?family=Roboto:900`
+// const url = `https://fonts.googleapis.com/css?family=Roboto:900`
+// const url = `https://fonts.googleapis.com/css?family=Roboto:300`
+const url = `https://fonts.googleapis.com/css?family=Roboto:400`
 
 googlefontcssmerge(url, function(error, style) {
   if (error) throw error
