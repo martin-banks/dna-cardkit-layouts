376cae42d27dace6cb15e921c666010c8d9810c2
martin-banks <anotherbanksy@gmail.com>
2018-10-25 17:00:29 +1100

    writes file b64 to local files

--- cardkit-master/getfonts.js
+++ cardkit-master/getfonts.js
@@ -1,18 +1,32 @@
 const googlefontcssmerge = require('googlefontcssmerge')
 const googlefontcss64 = require('googlefontcss64')
 const css = require('css')
+const fs = require('fs')
 
 // const url = `https://fonts.googleapis.com/css?family=Roboto:900`
 // const url = `https://fonts.googleapis.com/css?family=Roboto:300`
 // const url = `https://fonts.googleapis.com/css?family=Roboto:400`
-const url = `https://fonts.googleapis.com/css?family=Roboto+Condensed:400`
+// const url = `https://fonts.googleapis.com/css?family=Roboto+Condensed:400`
 
-googlefontcssmerge(url, function(error, style) {
-  if (error) throw error
+const urls = {
+  roboto900: `https://fonts.googleapis.com/css?family=Roboto:900`,
+  roboto300: `https://fonts.googleapis.com/css?family=Roboto:300`,
+  roboto400: `https://fonts.googleapis.com/css?family=Roboto:400`,
+  robotoCond400: `https://fonts.googleapis.com/css?family=Roboto+Condensed:400`,
+}
 
-  googlefontcss64(style, function(error, style) {
+for (const font in urls) {
+  console.log(font)
+  googlefontcssmerge(urls[font], async function(error, style) {
     if (error) throw error
-
-    console.log(css.stringify(style))
+    await googlefontcss64(style, async function(error, style) {
+      return new Promise(async (resolve, reject) => {
+        if (error) return reject(error)
+        await fs.writeFileSync(`${__dirname}/font_b64/${font}`, css.stringify(style))
+        console.log(`${font} written to file`)
+        resolve(`${font} written to file`)
+        // console.log(css.stringify(style))
+      })
+    })
   })
-})
+}
